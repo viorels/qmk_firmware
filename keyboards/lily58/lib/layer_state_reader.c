bd086d19e9cbc5065669905cfd89cb41542c6fa2
Viorel Stirbu <viorels@gmail.com>
2020-04-22 20:40:10 +0300

    show Colemak layer on display

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,9 +4,10 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
+#define L_COLEMAK (1 << 1)
+#define L_LOWER (1 << 2)
+#define L_RAISE (1 << 3)
+#define L_ADJUST (1 << 4)
 #define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
 
 char layer_state_str[24];
@@ -15,7 +16,10 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    if (default_layer_state == 1)
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
+    else if (default_layer_state == 2)
+      snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Colemak");
     break;
   case L_RAISE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
