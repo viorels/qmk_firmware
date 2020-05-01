21398638270a9115efa8f5ae1d27caab333723a7
Viorel Stirbu <viorels@gmail.com>
2020-05-01 16:58:05 +0300

    disable key logger on lcd

--- keyboards/lily58/keymaps/viorels/keymap.c
+++ keyboards/lily58/keymaps/viorels/keymap.c
@@ -228,7 +228,7 @@ void oled_task_user(void) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
     oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
