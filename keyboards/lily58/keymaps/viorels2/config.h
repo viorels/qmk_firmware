ee8131ef39015ea0f42de48bc8938c6ca82024d7
Viorel Stirbu <viorels@gmail.com>
2023-01-20 08:50:07 +0200

    increase tapping term

--- keyboards/lily58/keymaps/viorels2/config.h
+++ keyboards/lily58/keymaps/viorels2/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD_PER_KEY
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 150
+#define TAPPING_TERM 175
 #define TAPPING_TERM_PER_KEY
 #define TAPPING_TOGGLE 1
 
